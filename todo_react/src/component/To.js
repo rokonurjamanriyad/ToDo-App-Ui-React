4158a0355139a5431bb0542bc1f1ef3df94145ec
rokonurjamanriyad <rokonurjamanriyad@gmail.com>
2024-03-19 22:26:53 +0600

    complie

---
+++ todo_react/src/component/To.js
@@ -0,0 +1,28 @@
+import './To.css'
+import React from 'react';
+
+const To = () => {
+    return (
+        <div className='mmain'>
+            <div className="c1">
+                <input type="text" />
+                <input type="date" />
+                <button>Add</button>
+            </div>
+            <div className="c1">
+                <h3>name</h3>
+                <h3>name</h3>
+                <button>Delete</button>
+            </div>
+            <div className="c1">
+            <h3>name</h3>
+                <h3>name</h3>
+                <button>Delete</button>
+
+            </div>
+           
+        </div>
+    );
+};
+
+export default To;
